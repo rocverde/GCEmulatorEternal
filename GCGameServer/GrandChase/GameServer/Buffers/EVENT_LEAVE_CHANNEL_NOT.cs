64b86c1e553c9ebef0d2dd1a307b891487e21161
Hiro <hirohamada@post.com>
2017-07-18 22:48:24 -0300

    Add files via upload

---
+++ GCGameServer/GrandChase/GameServer/Buffers/EVENT_LEAVE_CHANNEL_NOT.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GameServer.network;
+
+namespace GameServer.Buffers
+{
+    class LeaveChannel
+    {
+        public void SairDoCanal(User user)
+        {
+            Ultilize.LobbyListaDePlayers.Remove(user);
+        }
+    }
+}
